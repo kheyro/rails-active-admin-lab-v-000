e11bc92f5270c766ef8c45b378c759aafc91ce65
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-20 13:19:12 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -23,17 +23,17 @@ class ArtistsController < ApplicationController
     @artist = Artist.find(params[:id])
   end
 
-  def update
-    @artist = Artist.find(params[:id])
-
-    @artist.update(artist_params)
-
-    if @artist.save
-      redirect_to @artist
-    else
-      render :edit
-    end
-  end
+  # def update
+  #   @artist = Artist.find(params[:id])
+  #
+  #   @artist.update(artist_params)
+  #
+  #   if @artist.save
+  #     redirect_to @artist
+  #   else
+  #     render :edit
+  #   end
+  # end
 
   def destroy
     @artist = Artist.find(params[:id])
